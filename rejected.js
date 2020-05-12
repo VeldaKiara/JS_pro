6a27d640fa7a29b4a5bad95cd440f13ad344302c
Velda Kiara <veldakarimi@gmail.com>
2020-05-12 20:59:05 +0300

    invoking  the handlers

--- rejected.js
+++ rejected.js
@@ -1,4 +1,4 @@
-const {checkInventory} = require('./library.js');
+const {checkInventory} = require('./promise.js');
 
 const order = [['sunglasses', 1], ['bags', 2]];
 
@@ -10,4 +10,5 @@ const handleSuccess=(resolvedValue) =>{
 const handleFailure =(rejectionReason)=>{
     console.log(rejectionReason)
   }
-  
+  checkInventory(order)
+  .then(handleSuccess, handleFailure);
