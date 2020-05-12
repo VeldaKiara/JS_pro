66f1ed2ebc94b2e2274933ea98fc7be1dd47a7f4
Velda Kiara <veldakarimi@gmail.com>
2020-05-12 20:52:19 +0300

    addition of failure function

--- rejected.js
+++ rejected.js
@@ -7,3 +7,7 @@ const order = [['sunglasses', 1], ['bags', 2]];
 const handleSuccess=(resolvedValue) =>{
   console.log(resolvedValue)
 }
+const handleFailure =(rejectionReason)=>{
+    console.log(rejectionReason)
+  }
+  
