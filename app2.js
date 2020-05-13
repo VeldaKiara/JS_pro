fd797259c0f034ef6e74b1fd7ccf5f3ef756dd03
Velda Kiara <veldakarimi@gmail.com>
2020-05-13 17:04:22 +0300

    added new filles and notes in docs

---
+++ app2.js
@@ -0,0 +1,14 @@
+const {checkAvailability} = require('./lib2.js');
+
+const onFulfill = (itemsArray) => {
+  console.log(`Items checked: ${itemsArray}`);
+  console.log(`Every item was available from the distributor. Placing order now.`);
+};
+
+const onReject = (rejectionReason) => {
+	console.log(rejectionReason);
+};
+
+
+
+
