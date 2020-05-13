f4614134fc4c5ba01bc49d795b9a204e29cf6e92
Velda Kiara <veldakarimi@gmail.com>
2020-05-13 17:01:20 +0300

    refactoring the code

--- app.js
+++ app.js
@@ -4,20 +4,21 @@ const order = {
   items: [['sunglasses', 1], ['bags', 2]],
   giftcardBalance: 79.82
 };
-
 checkInventory(order)
 .then((resolvedValueArray) => {
-    return processPayment(resolvedValueArray)
-
- 
+  return processPayment(resolvedValueArray);
 })
 .then((resolvedValueArray) => {
-    return shipOrder(resolvedValueArray)
-  
+  return shipOrder(resolvedValueArray);
 })
 .then((successMessage) => {
   console.log(successMessage);
 })
 .catch((errorMessage) => {
-  console.log(errorMessage);
-});
+    console.log(errorMessage);
+  });
+   
+
+
+
+
