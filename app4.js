318627e011f5bff69039f43d9ea961d69e4900a3
Velda Kiara <veldakarimi@gmail.com>
2020-05-13 20:59:48 +0300

    addition of new files

---
+++ app4.js
@@ -0,0 +1,18 @@
+const brainstormDinner = require('./lib4.js')
+
+
+// Native promise version:
+function nativePromiseDinner() {
+  brainstormDinner().then((meal) => {
+	  console.log(`I'm going to make ${meal} for dinner.`);
+  })
+}
+
+
+// async/await version:
+async function announceDinner() {
+  
+  
+  
+}
+
