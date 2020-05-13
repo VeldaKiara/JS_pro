ba303ba316dbe753bd6e941b3564bd9c6aa7d38f
Velda Kiara <veldakarimi@gmail.com>
2020-05-13 22:39:58 +0300

    addition of files using promises.all

---
+++ app9.js
@@ -0,0 +1,13 @@
+let {cookBeans, steamBroccoli, cookRice, bakeChicken} = require('./lib9.js')
+//using promises.all
+ async function serveDinnerAgain(){
+    let foodArray = await Promise.all([steamBroccoli(), cookRice(), bakeChicken(), cookBeans()]); 
+    
+    console.log(`Dinner is served. We're having ${foodArray[0]}, ${foodArray[1]}, ${foodArray[2]}, and ${foodArray[3]}.`)
+  }
+  
+  serveDinnerAgain()
+  
+
+
+
